18f2c0fd21d28f84eee09fe1e4b734d57f3d0ce6
Unknown <rhys.macraild@connect.qut.edu.au>
2018-07-28 00:48:54 +1000

    Made Better

--- BookSm@rt/app/popup.js
+++ BookSm@rt/app/popup.js
@@ -3,63 +3,55 @@ $(function () {
         var name = $("#txt_name").val();
 
         alert("Hi" + " " + name);
-        var html = document.documentElement.innerHTML;
-        var site = check_site(html);
+
+
+        let array_thingo = {};
+        let site_list = ['angus', 'qbd'];
+
+        let html = document.documentElement.innerHTML;
+        array_thingo = check_site(html, array_thingo);
+
+        for (var key in array_thingo ) {
+            if (object.hasOwnProperty(key)) {
+                if (!(key in site_list)){
+                //    Fetch Data and redo check_site thingy
+                }
+    }
+}
+
 
 
     });
 });
 
-function check_site(data) {
-    var angus_pattern = /www.angusrobertson.com.au/;
-    var qbd_pattern = /www.qbd.com.au/;
+//Determine which site the data is from and scrape appropriate book data
+function check_site(data, array_thingo) {
+    let angus_pattern = /www.angusrobertson.com.au/;
+    let qbd_pattern = /www.qbd.com.au/;
 
-    if (angus_pattern.test(data) == true) {
-        ISBN = get_ISBN_angus(html);
-        price = get_price_angus(html);
-        return 'angus';
+    //Scrape A&R data
+    if (angus_pattern.test(data) === true) {
+        angus_isbn = /sku">([0-9]*)/;
+        angus_price = /itemprop="price">([0-9.]*)/;
+        return scrape_page(angus_isbn, angus_price, 'angus', array_thingo)
 
-    } else if (qbd_pattern.test(data) == true) {
-        ISBN = get_ISBN_QBD(html);
-        price = get_price_QBD(html);
-        return 'qbd';
+    //Scrape QBD Data
+    } else if (qbd_pattern.test(data) === true) {
+        qdb_isbn = /isbn="([0-9]*)/;
+        qdb_price = /data-price="([0-9.]*)/;
+        return scrape_page(qdb_isbn, qdb_price, 'qbd', array_thingo)
 
     } else {
         console.log('Unable to identify site');
+        return array_thingo
     }
 
 }
 
-function get_ISBN_QBD(data) {
-    var pattern = /isbn="([0-9]*)/;
-    var match = pattern.exec(data);
-    return match;
+function scrape_page(data, isbn_pattern, price_pattern, identifier, array_thingo) {
+    let isbn = isbn_pattern.exec(data);
+    let price = price_pattern.exec(data);
+    array_thingo[identifier] = [isbn, price];
+    return array_thingo;
 }
 
-function get_ISBN_angus(data) {
-    var pattern = /sku">([0-9]*)/
-    var match = pattern.exec(data);
-    return match;
-}
-
-function get_price_QBD(data) {
-    var pattern = /data-price="([0-9.]*)/
-    var match = pattern.exec(data)
-    return match;
-}
-
-function get_price_angus(data) {
-    var pattern = /itemprop="price">([0-9.]*)/
-    var match = pattern.exec(data)
-    return match;
-}
-
-//function doalert() {
-//    alert("this");
-//    alert("that");
-//}
-
-//akjsdlajsdkajldjaslkdjaskld
-//My Test stuff
-
-//aklsjdlajskdjakljdl
