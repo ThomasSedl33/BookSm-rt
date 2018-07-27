9841a715cb768321e6efb924b919380e43b30b38
Unknown <rhys.macraild@connect.qut.edu.au>
2018-07-27 23:04:08 +1000

    Regex Stuff

--- BookSm@rt/app/popup.js
+++ BookSm@rt/app/popup.js
@@ -1,15 +1,36 @@
 $(function () {
-        $("#btnsave").click(function () {
-            //var name = $("#txt_name").val();
-    
-            //alert("Hi" + " " + name);
-            //get_ISBN_QDB('isbn="09090998374298')
-            alert('this');
+    $("#btnsave").click(function () {
+        var name = $("#txt_name").val();
 
-        });
+        alert("Hi" + " " + name);
+        var html = document.documentElement.innerHTML;
+        var site = check_site(html);
+
+
+    });
 });
 
-function get_ISBN_QDB(data) {
+function check_site(data) {
+    var angus_pattern = /www.angusrobertson.com.au/;
+    var qbd_pattern = /www.qbd.com.au/;
+
+    if (angus_pattern.test(data) == true) {
+        ISBN = get_ISBN_angus(html);
+        price = get_price_angus(html);
+        return 'angus';
+
+    } else if (qbd_pattern.test(data) == true) {
+        ISBN = get_ISBN_QBD(html);
+        price = get_price_QBD(html);
+        return 'qbd';
+
+    } else {
+        console.log('Unable to identify site');
+    }
+
+}
+
+function get_ISBN_QBD(data) {
     var pattern = /isbn="([0-9]*)/;
     var match = pattern.exec(data);
     return match;
@@ -21,7 +42,14 @@ function get_ISBN_angus(data) {
     return match;
 }
 
-function doalert()
-{
-    alert("Did it");
+function get_price_QBD(data) {
+    var pattern = /data-price="([0-9.]*)/
+    var match = pattern.exec(data)
+    return match;
+}
+
+function get_price_angus(data) {
+    var pattern = /itemprop="price">([0-9.]*)/
+    var match = pattern.exec(data)
+    return match;
 }
